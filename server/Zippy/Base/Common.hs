5af96eb0611dd6561ac5c4dc29983987f3c634a8
Ian Duncan <ian@iankduncan.com>
2013-02-18 12:07:15 -0800

    Add index helpers to Zippy.Base.Common

--- server/Zippy/Base/Common.hs
+++ server/Zippy/Base/Common.hs
@@ -22,4 +22,10 @@ keyProxy = const Proxy
 link :: Namespace a => ByteString -> Key a -> (Maybe ByteString, Maybe ByteString, Maybe ByteString)
 link rel k = (Just $ namespace $ keyProxy k, Just $ fromKey k, Just rel)
 
+index :: ByteString -> ByteString -> (ByteString, Maybe ByteString)
+index i v = (i, Just v)
+
+keyIndex :: ByteString -> Key a -> (ByteString, Maybe ByteString)
+keyIndex i k = index i $ fromKey k
+
 data Entity a = Entity { key :: Key a, value :: a }
