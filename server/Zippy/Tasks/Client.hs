af4539e7f0f9c9680dd407fbd959d832d7937c03
Ian Duncan <ian@iankduncan.com>
2013-02-18 12:06:41 -0800

    Add basic task client

--- server/Zippy/Tasks/Client.hs
+++ server/Zippy/Tasks/Client.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Zippy.Tasks.Client where
-import Network.HTTP.Conduit
+import Data.Monoid
+import Data.ByteString
+import Zippy.Base.Client
+import Zippy.Base.Common
+import Zippy.Tasks.Web.Models
 
+basicHandlers :: FromJSON a => [Handler a]
+basicHandlers = [ bodyOn 200, nothingOn 404 ]
 
+createTask :: NewTask -> ClientResult (Maybe Task)
+createTask = post "/tasks"
+    [ bodyOn 201
+    , nothingOn 404
+    ]
+
+getTask :: Key Task -> ClientResult (Maybe Task)
+getTask key = get ("/tasks/" <> keyToBS key) basicHandlers
+
+listUserTasks :: Key User -> ClientResult [Task]
+listUserTasks = undefined
+
+updateTask :: Key Task -> TaskChangeset -> ClientResult (Maybe Task)
+updateTask key = post ("/tasks/" <> keyToBS key) basicHandlers
