d9c4e09ab8568051f2eb94192a3e4bba1067c9ed
Ian Duncan <ian@iankduncan.com>
2013-02-18 12:03:11 -0800

    Add index function and clean out cruft from Zippy.Riak.Simple

--- server/Zippy/Riak/Simple.hs
+++ server/Zippy/Riak/Simple.hs
@@ -1,4 +1,5 @@
 module Zippy.Riak.Simple where
+import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.Proxy
 import Zippy.Base.Common
 import qualified Zippy.Riak.Content as C
@@ -13,12 +14,6 @@ import qualified Zippy.Riak.Object as O
 get :: (Namespace a, O.Get c, O.AsContent a) => Proxy a -> c -> Key a -> O.Riak b (C.GetResponse a)
 get p s k = O.get $ O.createGetRequest (namespace p) (fromKey k) s
 
---update :: O.Bucket -> O.Key -> a -> O.Riak b (C.PutResponse a)
---update = do
---	r <- O.get $ O.createGetRequest b k c
-	
-
---putNew :: (O.Put a, O.AsContent a, Semigroup a) => O.Bucket -> O.Key -> a -> O.Riak b (C.PutResponse a)
 put :: (Namespace a, O.Put c, O.AsContent a) => Proxy a -> c -> Key a -> a -> O.Riak b (C.PutResponse a)
 put p s k c = O.put $ O.createPutRequest (namespace p) (Just $ fromKey k) s c
 
@@ -27,3 +22,15 @@ putNew p s c = O.put $ O.createPutRequest (namespace p) Nothing s c
 
 delete :: (Namespace a, O.Delete c) => Proxy a -> c -> Key a -> O.Riak b ()
 delete p s k = O.delete $ O.createDeleteRequest (namespace p) (fromKey k) s
+
+class Indexable a where
+    indexEq :: (Namespace n) => Proxy n -> ByteString -> a -> O.Riak b [Key n]
+    indexRange :: (Namespace n) => Proxy n -> ByteString -> a -> a -> O.Riak b [Key n]
+
+instance Indexable ByteString where
+    indexEq p ix val = fmap (map Key) $ O.index (namespace p) ix $ O.StringQuery $ O.Key val
+    indexRange p ix low high = fmap (map Key) $ O.index (namespace p) ix $ O.StringQuery $ O.Range low high
+
+justOne :: [a] -> Maybe a
+justOne (x:[]) = Just x
+justOne _ = Nothing
